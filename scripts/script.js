a370044a3b7d7addada6e18a490168f3e9f51056
VelDevX <124770839+JohnVel27@users.noreply.github.com>
2025-02-20 08:00:20 -0800

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+// Display the current year in the footer
+document.addEventListener("DOMContentLoaded", function() {
+    var displayYear = document.getElementById('displayYear');
+    var currentYear = new Date().getFullYear();
+    displayYear.textContent = currentYear;
+  });
+
+  function toggleNavbar() {
+    const navbar = document.querySelector(".navbar");
+    const menuToggle = document.querySelector(".menu-toggle i");
+
+    navbar.classList.toggle("show");
+
+    // Toggle icon between bars and close
+    if (navbar.classList.contains("show")) {
+        menuToggle.classList.replace("fa-bars", "fa-times");
+    } else {
+        menuToggle.classList.replace("fa-times", "fa-bars");
+    }
+}
